16754758407d1523650e34350d33807073982c57
Favoredspecial <amarachiopara820@outlook.com>
2023-02-16 15:09:40 -0800

    Tasks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main .A Programming that prints a line with puts function
- * Return: 0 (Success)
+ * main -A Programming that prints a line with puts function
+ *
+ * Return: Always 0 (Success)
  **\
 int main(void)
 {
