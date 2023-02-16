c62948c52a0b17cc43d7a834797e4667ecd1ba22
Favoredspecial <amarachiopara820@outlook.com>
2023-02-16 14:51:55 -0800

    Tasks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main .A Programming that prints a line with puts function
  * Return: 0 (Success)
- *\
+ **\
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
