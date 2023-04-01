4cbc7ae2da1cd52f93b6602ad8f94046fe7063f2
Favoredspecial <amarachiopara820@outlook.com>
2023-03-31 18:38:30 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,4 +1,4 @@
-extern	printf		; the C function, to be called
+	extern	printf		; the C function, to be called
 
 	        section .data		; Data section, initialized variables
 msg:		db "Hello,University of Watrloo", 0 ; C string needs 0
